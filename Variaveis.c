29692954c77b2605a82631cee491fc715cec6826
João Paulo Delgado Preti <preti.joao@ifmt.edu.br>
2022-08-04 21:41:32 -0400

    exemplo com scanf

--- Variaveis.c
+++ Variaveis.c
@@ -7,11 +7,15 @@ int main() {
     float b = 10.5;
     char c = 'c';
     bool d = true;
-
+    printf("Endereco de memoria de a: %p\n", &a);
     printf("Valor de a: %d\n",a);
     printf("Valor de b: %f\n",b);
     printf("Valor de c: %c\n",c);
     printf("Valor de d: %d\n",d);
+
+    printf("Digite um valor para a: ");
+    scanf("%d", &a);
+    printf("Valor de a: %d\n",a);
 
     return EXIT_SUCCESS;
 }
