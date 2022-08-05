d7778ad583acead8bed22ed7ba1765993ae3e84e
João Paulo Delgado Preti <preti.joao@ifmt.edu.br>
2022-08-04 22:02:13 -0400

    Exemplo com leitura de texto

--- Variaveis.c
+++ Variaveis.c
@@ -7,15 +7,22 @@ int main() {
     float b = 10.5;
     char c = 'c';
     bool d = true;
+    char nome[50] = "";
+
     printf("Endereco de memoria de a: %p\n", &a);
     printf("Valor de a: %d\n",a);
-    printf("Valor de b: %f\n",b);
+    printf("Valor de b: %2.2f\n",b);
     printf("Valor de c: %c\n",c);
     printf("Valor de d: %d\n",d);
+    printf("Valor de nome: %s\n",nome);
+
+    printf("Nome completo: ");
+    scanf("%50[^\n]", nome);
+    printf("nome: %s\n",nome);
 
     printf("Digite um valor para a: ");
     scanf("%d", &a);
-    printf("Valor de a: %d\n",a);
+    printf("Novo valor de a: %d\n",a);   
 
     return EXIT_SUCCESS;
 }
