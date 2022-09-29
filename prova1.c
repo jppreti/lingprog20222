6f70be7464a09dd59150993716ec58a2599b5012
João Paulo Delgado Preti <preti.joao@ifmt.edu.br>
2022-09-29 18:55:08 -0400

    feat: prova 1

---
+++ prova1.c
@@ -0,0 +1,41 @@
+/*
+    PROVA PRÁTICA DE PYTHON
+    Ao término enviar e-mail conforme modelo:
+    Para:       preti.joao@ifmt.edu.br
+    Assunto:    Prova 1 de Linguagem de Programação 2022/2
+    Mensagem:   NOME COMPLETO DO ESTUDANTE
+    Anexo:      prova1.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    /*1. Faça um programa que leia o valor de um produto, o percentual
+         do desconto desejado e imprima o valor do desconto e o valor
+         do produto subtraindo o desconto. (2,5pt)*/
+
+
+    //2. Faça um programa que leia 3 números e imprima o maior deles. (2,5pt)
+
+
+    /*3. Faça um programa que permita entrar com o nome, a nota da prova 1 e a nota
+         da prova 2 de um aluno. O programa deve imprimir a média das notas e uma 
+         das mensagens: "Aprovado", "Reprovado" ou "em Prova Final"
+         (a média é 7 para aprovação, menor que 3 para reprovação e as demais em prova final). (2,5pt)*/
+
+
+    /*4. Faça um programa que apresente um menu com 4 opções:
+         [1] - Cadastrar
+         [2] - Excluir
+         [3] - Pesquisar
+         [0] - Sair
+         O usuário deve escolher uma opção e o programa deve imprimir uma mensagem 
+         ao entrar em cada opção do menu. O programa só deve encerrar quando a 
+         opção escolhida for zero (0). (2,5pt)*/
+
+
+
+    return EXIT_SUCCESS;
+}
