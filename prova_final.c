78088a278b5e6453f0d44b735ef866b250f54161
João Paulo Preti <jppreti@gmail.com>
2022-12-15 19:02:01 -0400

    feat: Prova Final

---
+++ prova_final.c
@@ -0,0 +1,23 @@
+/*
+    PROVA PRÁTICA DE C
+    Ao término enviar e-mail conforme modelo:
+    Para:       preti.joao@ifmt.edu.br
+    Assunto:    Prova Final de Linguagem de Programação 2022/2
+    Mensagem:   NOME COMPLETO DO ESTUDANTE
+    Anexo:      pf.c
+*/
+
+/*1. Faça um programa que efetue a apresentação do valor da conversão
+    em real (R$) de um valor lido em dólar (US$). Para isso, será
+    necessário também ler o valor da cotação do dólar. (2,5pt)*/
+
+/*2. Faça um programa que leia um número e imprima uma das mensagens:
+     "Maior do que 20", "Igual a 20" ou "Menor do que 20". (2,5pt)*/
+
+/*3. Faça um programa que leia um número inteiro e apresente o fatorial desse
+   número.
+   Ex.: o fatorial de 5 é igual a 5x4x3x2x1 = 120. (2,5pt)*/
+
+/*4. Implemente um programa em C que leia o nome, a idade e o endereço de uma
+     pessoa e armazene esses dados em uma estrutura (struct). Em seguida, imprima na tela
+     os dados da estrutura lida. (2,5pt)*/
